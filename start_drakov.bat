9e65ed7c16dc5e0d8acc28fcfec43f5cdc9a430e
mathiconical <netinho340@gmail.com>
2022-11-05 14:30:25 -0300

    AUTOMATIC get the port from example.apib and start drakov

---
+++ start_drakov.bat
@@ -0,0 +1,23 @@
+@echo off
+
+for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%%a"
+
+echo %ESC%[102m"* Create _host.txt file"%ESC%[0m
+find "HOST: " example.apib > _host.txt
+
+for /f "tokens=4 delims=:" %%a in ('type _host.txt^|find "HOST: "') do (
+  set port=%%a
+  goto :continue
+)
+:continue
+
+set port=%port%
+
+echo %ESC%[101m"* Remove _host.txt file"%ESC%[0m
+del _host.txt
+
+echo %ESC%[104m"* Runs drakov on port: "%ESC%[0m %ESC%[94m%port%%ESC%[0m
+timeout 1 > nul
+drakov -f example.apib -p %port%
+
+@echo on
